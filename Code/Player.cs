0b02e238156f4990a12d135f91a071451f9e9ff4
AngelofDeath666 <khejser@hotmail.com>
2022-10-28 13:29:23 +0200

    Started implementing player class

--- Code/Player.cs
+++ Code/Player.cs
@@ -1,9 +1,33 @@
 ﻿using System;
 
 public class Player
+	int pHealth, pArmour, pShottyAmmo, pArAmmo;
 {
 	public Player()	{
 
 
+
 	}
+
+	public static AddHealth (int health)
+	{
+		pHealth =+ health;
+	}
+
+	public static TakeDamage (int damage)
+	{
+		if (pArmour = 0)
+		{
+		pHealth -= damage;
+
+		} else
+		{
+			pArmour -= damage;
+		}
+		
+	}
+
+
+
+
 }
