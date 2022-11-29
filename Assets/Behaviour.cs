9306cba727380dcd21b4008d1ff7dcdb564c5aa4
AngelofDeath666 <khejser@hotmail.com>
2022-11-29 22:31:49 +0100

    Projectile tag added and collision detection added

--- Assets/Behaviour.cs
+++ Assets/Behaviour.cs
@@ -1,42 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Behaviour : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    public float rotationSpeed = 50f;
+    //public ProjectileBehaviour projectile1;
+    public GameObject projectilePrefab;
+
     // Update is called once per frame
     void Update()
     {
         Vector3 movementVector = Vector3.zero;
+        float rotationAngle = 0f;
+
+        if (Input.GetKey(KeyCode.Q)) {
+            rotationAngle -= 1;
+
+        }
+
+        if (Input.GetKey(KeyCode.E)) {
+            rotationAngle += 1;
+
+        }
+
         if (Input.GetKey(KeyCode.W))
         {
-            movementVector += new Vector3(1, 0, 0);     
-  
+            movementVector += transform.forward;     
+    
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            movementVector += new Vector3(-1, 0, 0);
+            movementVector -= transform.forward;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            movementVector += new Vector3(0, 0, -1);
+            movementVector += transform.right;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            movementVector += new Vector3(0, 0, 1);
+            movementVector -= transform.right;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(projectilePrefab);
+            projectile.GetComponent<ProjectileBehaviour>().Shoot(this);
+        }
+
 
         if (movementVector != Vector3.zero)
         {
+            
             movementVector = movementVector.normalized;
             transform.position = transform.position + movementVector * movementSpeed * Time.deltaTime;
-
         }
 
+     
+            transform.Rotate(new Vector3(0,rotationAngle*rotationSpeed* Time.deltaTime,0));
+            
     
     }
 }
---
+++ Assets/ProjectileBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    public float projectileDisPlayer = 1.5f;
+    public float projectileSpeed = 1000f;
+    public float projectileDespawn = 2f;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime > projectileDespawn)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public void Shoot (Behaviour shooter)
+    {
+        transform.position = shooter.transform.position + shooter.transform.forward * projectileDisPlayer;
+        GetComponent<Rigidbody>().AddForce(shooter.transform.forward*projectileSpeed);
+    }
+}
